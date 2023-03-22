51b5d55e3f2ea01039a91571ae9a48b8872c6084
emilioff8822 <emilioff@hotmail.it>
2023-03-22 17:12:44 +0100

    commit-appends

--- script.js
+++ script.js
@@ -1,39 +1,24 @@
 const list = document.querySelector('#list');
 
-
 for (let i = 1; i <= 100; i++) {
+  const li = document.createElement('li');
+
+  
   if (i % 3 === 0 && i % 5 === 0) {
-    
-   console.log("FizzBuzz");
-   list.innerHTML += "<li>FizzBuzz</li>";
- 
-
-
-
-
-
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-    list.innerHTML += "<li>Fizz</li>";
-
-  
-  
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-    list.innerHTML += "<li>Buzz</li>";
-
-
-  } else {
-    console.log(i);
-    list.innerHTML += "<li> " + i + "</li>";
-
-
+    li.append('FizzBuzz');
+  }
+  else if (i % 3 === 0) {
+    li.append('Fizz');
+  }
+  else if (i % 5 === 0) {
+    li.append('Buzz');
+  }
+  else {
+    li.append(i);
   }
 
-
-  
+  list.append(li);
 }
-
 
 
 
