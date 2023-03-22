66f0d825a019fb988a9af4ad3e2215666f8d88d7
emilioff8822 <emilioff@hotmail.it>
2023-03-22 18:35:05 +0100

    comm-quad-

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
 const list = document.querySelector('#list');
+const container = document.querySelector(".container");
+console.log(container)
 
 for (let i = 1; i <= 100; i++) {
   const li = document.createElement('li');
-  list.append(li);
+  //list.append(li);
+  
+  const box= document.createElement("div")
+  box.classList.add("box")
+  container.append(box)
+  box.append(i)
+
 
 
   
   if (i % 3 === 0 && i % 5 === 0) {
-    li.append('FizzBuzz');
+    //li.append('FizzBuzz');
+    box.classList.add('FizzBuzz');
+    box.innerText = "FizzBuzz";
+
+
   }
   else if (i % 3 === 0) {
-    li.append('Fizz');
+    //li.append('Fizz');
+    box.classList.add('Fizz');
+    box.innerText = "Fizz";
+
+
+
   }
   else if (i % 5 === 0) {
-    li.append('Buzz');
+    //li.append('Buzz');
+    box.classList.add('Buzz');
+    box.innerText = "Buzz";
+
+
+    
   }
   else {
     li.append(i);
