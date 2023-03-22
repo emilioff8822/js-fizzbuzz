d75d2eeb1bcb96281240ccf1abd2c817eebbaa05
emilioff8822 <emilioff@hotmail.it>
2023-03-22 17:16:10 +0100

    commit-vers2

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const list = document.querySelector('#list');
 
 for (let i = 1; i <= 100; i++) {
   const li = document.createElement('li');
+  list.append(li);
+
 
   
   if (i % 3 === 0 && i % 5 === 0) {
@@ -17,8 +19,48 @@ for (let i = 1; i <= 100; i++) {
     li.append(i);
   }
 
-  list.append(li);
 }
 
+
+/*INNER HTML
+
+const list = document.querySelector('#list');
+
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    
+   console.log("FizzBuzz");
+   list.innerHTML += "<li>FizzBuzz</li>";
+ 
+
+
+
+
+
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+    list.innerHTML += "<li>Fizz</li>";
+
+  
+  
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+    list.innerHTML += "<li>Buzz</li>";
+
+
+  } else {
+    console.log(i);
+    list.innerHTML += "<li> " + i + "</li>";
+
+
+  }
+
+
+  
+}
+
+
+*/
 
 
